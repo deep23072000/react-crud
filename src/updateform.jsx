19099c8bb86dedaa6f2e2fa475f7d39f62949553
deep23072000 <dp089901@gmail.com>
2024-10-19 14:18:52 +0530

    addd files

--- src/updateform.jsx
+++ src/updateform.jsx
@@ -1,28 +1,56 @@
+import axios from 'axios';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 const Updateform = () => {
+  let nav = useNavigate()
+  let [data,setData] = useState(
+    {
+      "name":"",
+      "email":""
+    }
+  );
     let {id} = useParams();
-    console.log(id)
+    console.log(id);
+
+
+    const handleinput = (e)=>{
+      let key = e.target.name;
+      let value = e.target.value;
+      setData({
+        ...data,
+        ...{
+          [key]:value
+        }
+      })
+
+      console.log(data)
+
+    }
+
+    const upd = ()=>{
+      axios.put(`http://localhost:4000/info/${id}`,JSON.stringify(data)).then(()=>{alert("update")}).then(()=>{nav("/")})
+     }
   return (
     <>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="email" name="name" value={data.name} placeholder="Enter name" onChange={handleinput} />
           <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
+            We'll never share your name with anyone else.
           </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" name="email" value={data.email} placeholder="Email" onChange={handleinput} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" onClick={upd}>
           Submit
         </Button>
       </Form>
